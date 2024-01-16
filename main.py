14128a905d723e368f703c91641677534d9943ff
V014 <wangakanjala@gmail.com>
2024-01-16 20:33:12 +0200

    finalized the project

--- main.py
+++ main.py
@@ -1,35 +1,39 @@
 import statistics # The standard python class
-import customtkinter
+import customtkinter # Import the theme class
 
 def std():
     try:
-        data = entry.get()
-        entries = data.split(',')
-        data_list = [int(x) for x in entries]
-        stdev_function = statistics.pstdev if population_var.get() else statistics.stdev
-        sample_std = statistics.stdev(data_list)
-        ans.configure(text=f"Sample standard deviation: {sample_std}")
-        
+        data = entry.get() # Get data from entry widget
+        entries = data.split(',') # Split the data from its commas ['1','2','3']
+        data_list = [int(x) for x in entries] # convert the data into int
+        stdev_function = statistics.pstdev if population_var.get() else statistics.stdev # determine standard deviation function 
+        std = stdev_function(data_list)
+
+        ans.configure(text=f"Standard deviation: {std}")
+
     except ValueError: ans.config(text="Invalid input")
 
 app = customtkinter.CTk()
-app.geometry("400x250")
+app.geometry("400x230")
 app.title("Standard Deviation Calculator")
 
 label = customtkinter.CTkLabel(app, text="Insert sample data")
 label.pack(padx=5, pady=5)
 
-entry = customtkinter.CTkEntry(app, placeholder_text="eg. 1,2,3,4,5")
-entry.pack(padx=5, pady=20)
-
 population_var = customtkinter.BooleanVar()
 toggle = customtkinter.CTkCheckBox(app, text="Population data", variable=population_var)
 toggle.pack()
 
+entry = customtkinter.CTkEntry(app, placeholder_text="eg. 1,2,3,4,5")
+entry.pack(padx=5, pady=20)
+
 calc_btn = customtkinter.CTkButton(app, text="Calculate", command=std)
-calc_btn.pack(padx=20, pady=20)
+calc_btn.pack(pady=10)
 
 ans = customtkinter.CTkLabel(app, text="")
-ans.pack(padx=10, pady=10)
+ans.pack()
+
+footer = customtkinter.CTkLabel(app, text="Orbit Media - Copyright© 2024")
+footer.pack()
 
 app.mainloop()
