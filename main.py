a176ec3c9df9ca1ff73eb755e1a3c8eaf3c9694c
V014 <wangakanjala@gmail.com>
2024-01-17 01:58:14 +0200

    adding coefficient of variance

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import statistics # The standard python class
+from statistics import stdev, pstdev, mean # The standard python class
 import customtkinter # Import the theme class
 
 def std():
@@ -6,19 +6,21 @@ def std():
         data = entry.get() # Get data from entry widget
         entries = data.split(',') # Split the data from its commas ['1','2','3']
         data_list = [int(x) for x in entries] # convert the data into int
-        stdev_function = statistics.pstdev if population_var.get() else statistics.stdev # determine standard deviation function 
-        std = stdev_function(data_list)
+        stdev_function = pstdev if population_var.get() else stdev # determine standard deviation function 
+        std = stdev_function(data_list) # set the selected function in a variable
+        mean_ = mean('data_list') # calculate the mean
+        cov = std / mean_ * 100 # calculate the coefficient of variation
 
-        ans.configure(text=f"Standard deviation: {std}")
+        std.configure(text=f"Standard deviation: {std}") # display the standard deviation
+        cv.configure(text=f"Coefficient of Variation: {cov:.2f}") # display the coefficient of variance
+    except ValueError: std.config(text="Invalid input") # in case of wrong input
 
-    except ValueError: ans.config(text="Invalid input")
+app = customtkinter.CTk() # declare the window, call it what you like, I went with app
+app.geometry("400x300") # declare the size
+app.title("Standard Deviation Calculator") # declare the title
 
-app = customtkinter.CTk()
-app.geometry("400x230")
-app.title("Standard Deviation Calculator")
-
-label = customtkinter.CTkLabel(app, text="Insert sample data")
-label.pack(padx=5, pady=5)
+label = customtkinter.CTkLabel(app, text="Insert sample data") # declare a label to let the user know wha to do
+label.pack(padx=5, pady=5) # apply it to the window
 
 population_var = customtkinter.BooleanVar()
 toggle = customtkinter.CTkCheckBox(app, text="Population data", variable=population_var)
@@ -30,8 +32,10 @@ entry.pack(padx=5, pady=20)
 calc_btn = customtkinter.CTkButton(app, text="Calculate", command=std)
 calc_btn.pack(pady=10)
 
-ans = customtkinter.CTkLabel(app, text="")
-ans.pack()
+std = customtkinter.CTkLabel(app, text="")
+std.pack()
+
+cv = customtkinter.CTkLabel(app, text="")
 
 footer = customtkinter.CTkLabel(app, text="Orbit Media - Copyright© 2024")
 footer.pack()
